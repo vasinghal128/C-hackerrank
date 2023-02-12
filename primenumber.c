354d78dcd91d5f29ee14cd6fa7f8f74712f9071a
VAIBHAV SINGHAL <125060733+vasinghal128@users.noreply.github.com>
2023-02-12 17:51:08 +0530

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i=1,a;
+    printf("Enter Any Number");
+    scanf("%d",&a);
+    
+    while(i<=a)
+    
+    {
+    	
+    	i++ ;
+    	
+    	
+	}
+    
+    printf ("no is prime ");
+    
+   
+    
+
+return 0;
+}
