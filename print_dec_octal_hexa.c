677d02becf28f4438f9c640e845a5ba9582c3786
VAIBHAV SINGHAL <125060733+vasinghal128@users.noreply.github.com>
2023-02-12 17:51:43 +0530

    Add files via upload

---
+++ print_dec_octal_hexa.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    int n ;
+    printf("Enter the value");
+    scanf("%d" , &n);
+    printf("%d\n" , n);
+    printf("%o\n" , n);
+    printf("%x\n" , n);
+    printf("%X\n" , n);
+    return 0;
+}
+
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    int n ;
+    printf("Enter the value");
+    scanf("%d" , &n);
+    printf("%d" , n);
+    printf("%o" , n);
+    printf("%x" , n);
+    printf("%X" , n);
+    return 0;
+}
+
